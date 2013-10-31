32d1240b498801a128d7dacb7d1da3b438cf9913
Blake Rego <blake.rego@gmail.com>
2013-10-31 12:04:30 +0700

    showing tiles by group on home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
 
 	def index
-		@tiles = Tile.published
     group_id = params[:group_id]
     @group = group_id.present? ? Group.find(group_id) : Group.first
+    @tiles = @group.tiles
 	end
 
 end
